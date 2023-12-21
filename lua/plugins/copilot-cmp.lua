5cb7644721bc25d6d2eb4b35225306e3a0e05e8a
yonatanlevy <yonatan.levy@avalor.io>
2023-12-21 17:06:33 +0200

    first commit

---
+++ lua/plugins/copilot-cmp.lua
@@ -0,0 +1,27 @@
+return {
+  {
+    "zbirenbaum/copilot-cmp",
+    dependencies = "copilot.lua",
+    opts = {},
+    config = function(_, opts)
+      local copilot_cmp = require("copilot_cmp")
+      -- local lsp = require("lsp-zero")
+      -- lsp.setup_nvim_cmp({
+      --   preselect = "none",
+      --   completion = {
+      --     completeopt = "menu,menuone,noselect",
+      --   },
+      -- })
+      -- lsp.setup()
+      copilot_cmp.preselect = require("cmp.types").cmp.PreselectMode.None
+      copilot_cmp.setup(opts)
+      -- attach cmp source whenever copilot attaches
+      -- fixes lazy-loading issues with the copilot cmp source
+      require("lazyvim.util").lsp.on_attach(function(client)
+        if client.name == "copilot" then
+          copilot_cmp._on_insert_enter({})
+        end
+      end)
+    end,
+  },
+}
